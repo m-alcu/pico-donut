ed56911686ab5662f0e3dc44a938b3ef1ee63d30
Martin Alcubierre <martin.alcubierre@gmail.com>
2025-07-27 18:34:16 +0200

    Refactor

--- colorlcd_240x240_demo.cpp
+++ colorlcd_240x240_demo.cpp
@@ -13,16 +13,12 @@ using namespace torus3d;
 const int WIDTH = 240;
 const int HEIGHT = 240;
 
-#define MAX_OBJ 12
-#define NLINES 240
+#define BUTTON_PIN_A 12
+#define BUTTON_PIN_B 13
+#define BUTTON_PIN_X 14
+#define BUTTON_PIN_Y 15
 
-#define SCR_WD  240
-#define SCR_HT  240
-#define WD_3D   240
-#define HT_3D   240
-#define BUTTON_PIN 15
-
-uint16_t frBuf[SCR_WD*NLINES];
+uint16_t frBuf[WIDTH*HEIGHT];
 uint16_t color565_table[256];
 
 volatile bool core0_done = false;
@@ -30,15 +26,6 @@ volatile bool core1_done = false;
 
 ST7789 st7789(WIDTH, HEIGHT, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
 PicoGraphics_PenRGB565 graphics(st7789.width, st7789.height, frBuf);
-
-int buttonState;
-int prevState = true;
-long btDebounce    = 30;
-long btMultiClick  = 600;
-long btLongClick   = 500;
-long btLongerClick = 2000;
-long btTime = 0, btTime2 = 0;
-int clickCnt = 1;
 
 void core1_main() {
 
@@ -55,51 +42,22 @@ void core1_main() {
 
     while (true) {
 
-        printf("core1\n");
-
         drawTorus(angles, 0, 120*240, frBuf, color565_table);
         core1_done = true;
         // Wait for core0 to finish
         while (!core0_done) tight_loop_contents();
 
-        // (Optional) Core 1 does nothing else, or could help with next frame
+        //Core1 does not update the screen, it just draws the first half of the torus
     }
 }
 
-// 0=idle, 1,2,3=click, -1,-2=longclick
-int checkButton( bool state)
-{
-  if( state == false && prevState == true ) { btTime = millis(); prevState = state; return 0; } // button just pressed
-  if( state == true && prevState == false ) { // button just released
-    prevState = state;
-    if( millis()-btTime >= btDebounce && millis()-btTime < btLongClick ) { 
-      if( millis()-btTime2<btMultiClick ) clickCnt++; else clickCnt=1;
-      btTime2 = millis();
-      return clickCnt; 
-    } 
-  }
-  if( state == false && millis()-btTime >= btLongerClick ) { prevState = state; return -2; }
-  if( state == false && millis()-btTime >= btLongClick ) { prevState = state; return -1; }
-  return 0;
-}
-
-int prevButtonState=0;
-
-int handleButton(bool button)
-{
-  prevButtonState = buttonState;
-  buttonState = checkButton(button);
-  return buttonState;
-}
-
-unsigned int ms, msMin=1000, msMax=0, stats=1;
+unsigned int ms, msMin=1000, msMax=0;
 
 void showStats() {
-    Pen WHITE   = graphics.create_pen(255, 255, 255);
+
     Pen YELLOW  = graphics.create_pen(255, 255, 0);
     Pen GREEN   = graphics.create_pen(0, 255, 0);
     Pen MAGENTA = graphics.create_pen(255, 0, 255);
-    Pen BLACK   = graphics.create_pen(0, 0, 0);
 
     char txt[30];
  
@@ -108,36 +66,21 @@ void showStats() {
 
     snprintf(txt, 30, "%d ms     %d fps ", ms, 1000 / ms);
     graphics.set_pen(YELLOW);
-    graphics.text(txt, Point(0, SCR_HT - 32), 240);
+    graphics.text(txt, Point(0, HEIGHT - 32), 240);
 
     snprintf(txt, 30, "%d-%d ms  %d-%d fps   ", msMin, msMax, 1000 / msMax, 1000 / msMin);
     graphics.set_pen(GREEN);
-    graphics.text(txt, Point(0, SCR_HT - 22), 240);
+    graphics.text(txt, Point(0, HEIGHT - 22), 240);
 
     snprintf(txt, 30, "r1: %d r2: %d", iter1, iter2);
     graphics.set_pen(MAGENTA);
-    graphics.text(txt, Point(0, SCR_HT - 12), 240);
+    graphics.text(txt, Point(0, HEIGHT - 12), 240);
 
 }
 
 int main() {
 
   stdio_init_all();
-  multicore_launch_core1(core1_main);  
-
-  st7789.set_backlight(255);
-
-  gpio_init(BUTTON_PIN);
-  gpio_set_dir(BUTTON_PIN, GPIO_IN);
-  gpio_pull_up(BUTTON_PIN); // Use pull-up if button connects to GND
-
-  bool last_button = true;
-
-  //precopumpute color565 table
-
-  for (int c = 0; c < 256; ++c) {
-      color565_table[c] = pimoroni::RGB(c, c, 0).to_rgb565();
-  }
 
   torus3d::TorusAngles angles;
 
@@ -150,6 +93,32 @@ int main() {
   angles.sAcB = 11583; 
   angles.cAcB = 11583;      
 
+  //precopumpute color565 table
+  for (int c = 0; c < 256; ++c) {
+      color565_table[c] = pimoroni::RGB(c, c, 0).to_rgb565();
+  }  
+
+  multicore_launch_core1(core1_main);  
+
+  st7789.set_backlight(255);
+
+  gpio_init(BUTTON_PIN_A);
+  gpio_set_dir(BUTTON_PIN_A, GPIO_IN);
+  gpio_pull_up(BUTTON_PIN_A); // Use pull-up if button connects to GND
+  
+  gpio_init(BUTTON_PIN_B);
+  gpio_set_dir(BUTTON_PIN_B, GPIO_IN);
+  gpio_pull_up(BUTTON_PIN_B);
+  
+  gpio_init(BUTTON_PIN_X);
+  gpio_set_dir(BUTTON_PIN_X, GPIO_IN);
+  gpio_pull_up(BUTTON_PIN_X);
+
+  gpio_init(BUTTON_PIN_Y);
+  gpio_set_dir(BUTTON_PIN_Y, GPIO_IN);
+  gpio_pull_up(BUTTON_PIN_Y);
+
+  
   while(true) {
 
     //printf("core0");
@@ -157,24 +126,18 @@ int main() {
     core0_done = false;
     core1_done = false;    
 
-    bool button = gpio_get(BUTTON_PIN);
-    handleButton(button);
-    if(buttonState<0) {
-        if(buttonState==-2 && prevButtonState==-1) {
-        stats=!stats; 
-        }
-    } else
-    if(buttonState>0) {
-        msMin=1000;
-        msMax=0;
-    }
+    bool button_a = gpio_get(BUTTON_PIN_A);
+    bool button_b = gpio_get(BUTTON_PIN_B);
+    bool button_x = gpio_get(BUTTON_PIN_X);
+    bool button_y = gpio_get(BUTTON_PIN_Y);
+
 
     ms=millis();
 
     drawTorus(angles, 120*240, 240*240, frBuf, color565_table);
     
     ms=millis()-ms;
-    if(stats) showStats();
+    showStats();
 
     while (!core1_done) tight_loop_contents();
 
