b3662e354dbf6500d40322ffab8cb87110dbc3e2
Martin Alcubierre <martin.alcubierre@gmail.com>
2025-07-27 13:18:02 +0200

    Optimize to 12 fps

--- colorlcd_240x240_demo.cpp
+++ colorlcd_240x240_demo.cpp
@@ -5,8 +5,10 @@
 #include "drivers/st7789/st7789.hpp"
 #include "libraries/pico_graphics/pico_graphics.hpp"
 #include "gfx3d.h"
+#include "pico/multicore.h"
 
 using namespace pimoroni;
+using namespace torus3d;
 
 const int WIDTH = 240;
 const int HEIGHT = 240;
@@ -23,6 +25,9 @@ const int HEIGHT = 240;
 uint16_t frBuf[SCR_WD*NLINES];
 uint16_t color565_table[256];
 
+volatile bool core0_done = false;
+volatile bool core1_done = false;
+
 ST7789 st7789(WIDTH, HEIGHT, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
 PicoGraphics_PenRGB565 graphics(st7789.width, st7789.height, frBuf);
 
@@ -34,6 +39,32 @@ long btLongClick   = 500;
 long btLongerClick = 2000;
 long btTime = 0, btTime2 = 0;
 int clickCnt = 1;
+
+void core1_main() {
+
+    torus3d::TorusAngles angles;
+
+    angles.sB = 0;     
+    angles.cB = 16384;
+    angles.sA = 11583; 
+    angles.cA = 11583;
+    angles.sAsB = 0;   
+    angles.cAsB = 0;
+    angles.sAcB = 11583; 
+    angles.cAcB = 11583;       
+
+    while (true) {
+
+        printf("core1\n");
+
+        drawTorus(angles, 0, 120*240, frBuf, color565_table);
+        core1_done = true;
+        // Wait for core0 to finish
+        while (!core0_done) tight_loop_contents();
+
+        // (Optional) Core 1 does nothing else, or could help with next frame
+    }
+}
 
 // 0=idle, 1,2,3=click, -1,-2=longclick
 int checkButton( bool state)
@@ -77,15 +108,23 @@ void showStats() {
 
     snprintf(txt, 30, "%d ms     %d fps ", ms, 1000 / ms);
     graphics.set_pen(YELLOW);
-    graphics.text(txt, Point(0, SCR_HT - 22), 240);
+    graphics.text(txt, Point(0, SCR_HT - 32), 240);
 
     snprintf(txt, 30, "%d-%d ms  %d-%d fps   ", msMin, msMax, 1000 / msMax, 1000 / msMin);
     graphics.set_pen(GREEN);
+    graphics.text(txt, Point(0, SCR_HT - 22), 240);
+
+    snprintf(txt, 30, "r1: %d r2: %d", iter1, iter2);
+    graphics.set_pen(MAGENTA);
     graphics.text(txt, Point(0, SCR_HT - 12), 240);
 
 }
 
 int main() {
+
+  stdio_init_all();
+  multicore_launch_core1(core1_main);  
+
   st7789.set_backlight(255);
 
   gpio_init(BUTTON_PIN);
@@ -99,8 +138,24 @@ int main() {
   for (int c = 0; c < 256; ++c) {
       color565_table[c] = pimoroni::RGB(c, c, 0).to_rgb565();
   }
-  
+
+  torus3d::TorusAngles angles;
+
+  angles.sB = 0;     
+  angles.cB = 16384;
+  angles.sA = 11583; 
+  angles.cA = 11583;
+  angles.sAsB = 0;   
+  angles.cAsB = 0;
+  angles.sAcB = 11583; 
+  angles.cAcB = 11583;      
+
   while(true) {
+
+    //printf("core0");
+
+    core0_done = false;
+    core1_done = false;    
 
     bool button = gpio_get(BUTTON_PIN);
     handleButton(button);
@@ -115,12 +170,17 @@ int main() {
     }
 
     ms=millis();
-    drawTorus(frBuf, color565_table);
+
+    drawTorus(angles, 120*240, 240*240, frBuf, color565_table);
+    
     ms=millis()-ms;
     if(stats) showStats();
 
+    while (!core1_done) tight_loop_contents();
+
     // update screen
     st7789.update(&graphics);
+    core0_done = true;
 
     }
     
--- gfx3d.h
+++ gfx3d.h
@@ -12,12 +12,22 @@ const int SCREEN_WIDTH = 240;
 const int SCREEN_HEIGHT = 240;
 const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
 
-int16_t sB = 0, cB = 16384;
-int16_t sA = 11583, cA = 11583;
-int16_t sAsB = 0, cAsB = 0;
-int16_t sAcB = 11583, cAcB = 11583;
+int iter1 = 8, iter2 = 8;
 
-int iter1 = 3, iter2 = 2;
+namespace torus3d {
+
+    struct TorusAngles {
+        int16_t sB;
+        int16_t cB;
+        int16_t sA;
+        int16_t cA;
+        int16_t sAsB;
+        int16_t cAsB;
+        int16_t sAcB; 
+        int16_t cAcB;
+    };
+
+}
 
 // CORDIC algorithm (calculate the length of a vector https://en.wikipedia.org/wiki/CORDIC)
 inline int length_cordic(int16_t x, int16_t y, int16_t *x2_, int16_t y2, int16_t iterations = 8) {
@@ -45,107 +55,105 @@ inline int length_cordic(int16_t x, int16_t y, int16_t *x2_, int16_t y2, int16_t
     return (x >> 1) + (x >> 3);
 }
 
-// animated checkerboard pattern
-void backgroundChecker(int sA, int sB, uint16_t* frBuf)
-{
-  int x,y,xx,yy, xo,yo;
-  xo = (sB >> 9)+50;
-  yo = (sA >> 9)+50;
+void drawTorus(torus3d::TorusAngles& angles, int fromLine, int toLine, uint16_t* frBuf, uint16_t* color565_table) {
 
-  int hy = 0;
-
-  for(y=0;y<SCREEN_HEIGHT;y++, hy+= SCREEN_WIDTH) {
-    yy = (y+yo) % 64;
-    for(x=0;x<SCREEN_WIDTH;x++) {
-      xx = (x+xo) % 64;
-      frBuf[hy+x] = ((xx<32 && yy<32) || (xx>32 && yy>32)) ? BLUE : GREY;
-    }
-  }
-}
-
-void drawTorus(uint16_t* frBuf, uint16_t* color565_table) {
-
-    backgroundChecker(sA, sB, frBuf);
+    //backgroundChecker(angles.sA, angles.sB, frBuf);
 
     // Don't worry, even though this looks like a multiplication, 
     // the compiler will probably optimize it into shifts 
     // and adds because dz is a known constant (5). 
-    int p0x = dz * sB >> 6;
-    int p0y = dz * sAcB >> 6;
-    int p0z = -dz * cAcB >> 6;
+    int p0x = dz * angles.sB >> 6;
+    int p0y = dz * angles.sAcB >> 6;
+    int p0z = -dz * angles.cAcB >> 6;
 
     const int r1i = r1 * 256;
     const int r2i = r2 * 256;
 
     int niters = 0;
     int nnormals = 0;
-    int16_t yincC = (cA >> 6);
-    int16_t yincS = (sA >> 6);
+    int16_t yincC = (angles.cA >> 6);
+    int16_t yincS = (angles.sA >> 6);
 
-    int16_t xincX = (cB >> 6);
-    int16_t xincY = (sAsB >> 6);
-    int16_t xincZ = (cAsB >> 6);
+    int16_t xincX = (angles.cB >> 6);
+    int16_t xincY = (angles.sAsB >> 6);
+    int16_t xincZ = (angles.cAsB >> 6);
         
-    int16_t ycA = -cA;
-    int16_t ysA = -sA;
+    int16_t ycA = -angles.cA;
+    int16_t ysA = -angles.sA;
 
-    for (int j = 0; j < SCREEN_SIZE; j +=(SCREEN_WIDTH << 1), ycA += yincC, ysA += yincS) {
-        int xsAsB = (sAsB >> 4) - sAsB;
-        int xcAsB = (cAsB >> 4) - cAsB;
+    int jb = 0;
 
-        int16_t vxi14 = (cB >> 4) - cB - sB;
-        int16_t vyi14 = ycA - xsAsB - sAcB;
-        int16_t vzi14 = ysA + xcAsB + cAcB;
+    for (int j = 0; j < SCREEN_SIZE; j +=(SCREEN_WIDTH << 1), ycA += yincC, ysA += yincS, jb +=2) {
+        int xsAsB = (angles.sAsB >> 4) - angles.sAsB;
+        int xcAsB = (angles.cAsB >> 4) - angles.cAsB;
+
+        int16_t vxi14 = (angles.cB >> 4) - angles.cB - angles.sB;
+        int16_t vyi14 = ycA - xsAsB - angles.sAcB;
+        int16_t vzi14 = ysA + xcAsB + angles.cAcB;
+
+        int jbx = jb % 64;
 
         for (int i = 0; i < SCREEN_WIDTH; i +=2, vxi14 += xincX, vyi14 -= xincY, vzi14 += xincZ) {
             int t = 512; // (256 * dz) - r2i - r1i;
+
+            int ix = i % 64;
 
             int16_t px = p0x + (vxi14 >> 5); // assuming t = 512, t*vxi>>8 == vxi<<1
             int16_t py = p0y + (vyi14 >> 5);
             int16_t pz = p0z + (vzi14 >> 5);
 
-            int16_t lx0 = sB >> 2;
-            int16_t ly0 = sAcB - cA >> 2;
-            int16_t lz0 = -cAcB - sA >> 2;
+            int16_t lx0 = angles.sB >> 2;
+            int16_t ly0 = angles.sAcB - angles.cA >> 2;
+            int16_t lz0 = -angles.cAcB - angles.sA >> 2;
 
-            for (;;) {
-                int t0, t1, t2, d;
-                int16_t lx = lx0, ly = ly0, lz = lz0;
-                t0 = length_cordic(px, py, &lx, ly, iter1);
-                t1 = t0 - r2i;
-                t2 = length_cordic(pz, t1, &lz, lx, iter2);
-                d = t2 - r1i;
-                t += d;
+            if (j >= fromLine && j < toLine) {
+                for (;;) {
+                    int t0, t1, t2, d;
+                    int16_t lx = lx0, ly = ly0, lz = lz0;
+                    t0 = length_cordic(px, py, &lx, ly, iter1);
+                    t1 = t0 - r2i;
+                    t2 = length_cordic(pz, t1, &lz, lx, iter2);
+                    d = t2 - r1i;
+                    t += d;
 
-                if (t > 8 * 256) {
-                    break;
-                } else if (d < 3) {
-                    int N = lz >> 5;
-                    uint16_t color = color565_table[(N > 0 ? N < 256 ? N : 255 : 0)];
                     int index = j + i;
-                    frBuf[index] = color;
-                    frBuf[index+1] = color;
-                    frBuf[index+SCREEN_WIDTH] = color;
-                    frBuf[index+SCREEN_WIDTH+1] = color;
-                    nnormals++;
-                    break;
-                }
+                    if (t > 8 * 256) {
+                        uint16_t color = ((jbx<32 && ix<32) || (jbx>32 && ix>32)) ? BLUE : GREY;
+                        frBuf[index] = color;
+                        frBuf[index+1] = color;
+                        frBuf[index+SCREEN_WIDTH] = color;
+                        frBuf[index+SCREEN_WIDTH+1] = color;
+                        break;
+                    } else if (d < 3) {
+                        int N = lz >> 5;
+                        uint16_t color = color565_table[(N > 0 ? N < 256 ? N : 255 : 0)];
+                        int index = j + i;
+                        frBuf[index] = color;
+                        frBuf[index+1] = color;
+                        frBuf[index+SCREEN_WIDTH] = color;
+                        frBuf[index+SCREEN_WIDTH+1] = color;
+                        nnormals++;
+                        break;
+                    }
 
-                px += d * vxi14 >> 14;
-                py += d * vyi14 >> 14;
-                pz += d * vzi14 >> 14;
-                niters++;
+                    px += d * vxi14 >> 14;
+                    py += d * vyi14 >> 14;
+                    pz += d * vzi14 >> 14;
+                    niters++;
+                }
             }
+
         }
     }
 
     // rotate sines, cosines, and products thereof
     // this animates the torus rotation about two axes
-    R(5, cA, sA);
-    R(5, cAsB, sAsB);
-    R(5, cAcB, sAcB);
-    R(6, cB, sB);
-    R(6, cAcB, cAsB);
-    R(6, sAcB, sAsB);
+    R(5, angles.cA, angles.sA);
+    R(5, angles.cAsB, angles.sAsB);
+    R(5, angles.cAcB, angles.sAcB);
+    R(6, angles.cB, angles.sB);
+    R(6, angles.cAcB, angles.cAsB);
+    R(6, angles.sAcB, angles.sAsB);
+    //printf("cA: %d, sA: %d\n", angles.cA, angles.sA);
 
 }
